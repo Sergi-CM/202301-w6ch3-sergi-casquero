7d9875804060f1755960766aacf16a7f11785cb3
Sergi Casquero <sergicasquero@gmail.com>
2023-02-15 22:09:54 +0100

    Add questions and winner functions

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import chalk from "chalk";
 import inquirer from "inquirer";
 import { Command } from "commander";
+import { createSpinner } from "nanospinner";
 
-let playerName;
+let playerName: string;
+
+const timeOut = (milliseconds = 2000) =>
+  new Promise((action) => setTimeout(action, milliseconds));
 
 const welcome = async (): Promise<void> => {
   console.log(`
@@ -10,8 +14,8 @@ const welcome = async (): Promise<void> => {
 
   console.log(`
     ${chalk.blue("HOW TO PLAY")}
-    I am a virtual life inside your computer.
-    If you get any answer wrong I will ${chalk.red("DIE")}.
+    You will have to answer 3 questions.
+    If you get any answer wrong you will ${chalk.red("DIE")}.
     So you better answer all questions correctly!
   `);
 };
@@ -31,4 +35,60 @@ const askName = async (): Promise<void> => {
   playerName = answers["player-name"];
 };
 
+const question1 = async (): Promise<void> => {
+  const answers = await inquirer.prompt({
+    name: "question1",
+    type: "list",
+    message: "How many days have passed since the first day of this bootcamp?",
+    choices: ["35", "37", "39", "41"],
+  });
+
+  return handleAnswer(answers.question1 === "39");
+};
+
+const question2 = async (): Promise<void> => {
+  const answers = await inquirer.prompt({
+    name: "question2",
+    type: "list",
+    message: "What does 'b' point to?: a=5, b=a, a=7",
+    choices: ["5", "7", "a", "undefined"],
+  });
+
+  return handleAnswer(answers.question2 === "5");
+};
+
+const question3 = async (): Promise<void> => {
+  const answers = await inquirer.prompt({
+    name: "question3",
+    type: "list",
+    message: "What's the return of this expression?: !danAbramov",
+    choices: ["Andrew Smith", "Ander Clark", "Andrew Clark", "Clark Andrews"],
+  });
+
+  return handleAnswer(answers.question3 === "Andrew Clark");
+};
+
+const handleAnswer = async (isCorrect: boolean) => {
+  const spinner = createSpinner("Checking answer...").start();
+  await timeOut();
+
+  if (isCorrect) {
+    spinner.success({ text: `Nice work ${playerName}.` });
+  } else {
+    spinner.error({ text: `💀 Game over! 💀` });
+    process.exit(1);
+  }
+};
+
+function winner() {
+  console.clear();
+  console.log(`Congrats , ${playerName}!`);
+}
+
+console.clear();
+await welcome();
 await askName();
+await question1();
+await question2();
+await question3();
+winner();
